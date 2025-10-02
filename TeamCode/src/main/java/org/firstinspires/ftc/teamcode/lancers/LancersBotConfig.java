54c314d3d8e0588e79aec1aab3410548e691428b
alm0st01 <alanroyjr@gmail.com>
2025-10-02 17:03:49 -0400

    First commit for decode season

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersBotConfig.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.lancers;
+
+public final class LancersBotConfig {
+
+    //Motor configurations from the hardware map
+    public static final String FRONT_LEFT_MOTOR = "frontLeftMotor";
+    public static final String REAR_LEFT_MOTOR = "rearLeftMotor";
+    public static final String FRONT_RIGHT_MOTOR = "frontRightMotor";
+    public static final String REAR_RIGHT_MOTOR = "rearRightMotor";
+    public final static String INTAKE_MOTOR = "intakeMotor";
+    public final static String IMU = "imu";
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersTeleOp.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode.lancers;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp()
+public class LancersTeleOp extends LinearOpMode {
+    public static final String TAG = "LancerTeleOp";
+    private long currentRunTimeStamp = -1;
+    private long timeStampAtLastOpModeRun = -1;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException  {
+        // Get from hardwaremap, initialize variables as DcMotor type
+        final DcMotor leftFront = hardwareMap.dcMotor.get(LancersBotConfig.FRONT_LEFT_MOTOR);
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        final DcMotorEx leftRear = (DcMotorEx) hardwareMap.dcMotor.get(LancersBotConfig.REAR_LEFT_MOTOR);
+        leftRear.setDirection(DcMotorSimple.Direction.FORWARD);
+        final DcMotor rightFront = hardwareMap.dcMotor.get(LancersBotConfig.FRONT_RIGHT_MOTOR);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        final DcMotor rightRear = hardwareMap.dcMotor.get(LancersBotConfig.REAR_RIGHT_MOTOR);
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //intake
+        final DcMotor intakeMotor = hardwareMap.dcMotor.get(LancersBotConfig.INTAKE_MOTOR);
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //DcMotorEx inherits from DcMotor class,
+        //DcMotorEx used in pretty much the same way as DcMotor
+        //I honestly don't really know the difference, but DcMotorEx seems to have more functionality
+
+        //Declaring odometry pods/dead wheels/encoders/whatever you want to call it
+        //Pulling from hardware map again
+        //parallelEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, LancersBotConfig.FRONT_RIGHT_MOTOR));
+        //perpendicularEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, LancersBotConfig.FRONT_LEFT_MOTOR));
+
+        // go!!
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            currentRunTimeStamp = System.currentTimeMillis();
+
+            // movement
+            final double speedMultiplier = gamepad1.a ? 1.0d : 0.8d;
+
+            // Gamepad positions; Motors are swapped
+            final double ly = -respectDeadZones(gamepad1.left_stick_y) * speedMultiplier; // Remember, Y stick value is reversed
+            final double lx = -respectDeadZones(gamepad1.left_stick_x) * speedMultiplier; // Counteract imperfect strafing
+            final double rx = respectDeadZones(gamepad1.right_stick_x) * speedMultiplier;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            final double denominator = Math.max(Math.abs(ly) + Math.abs(lx) + Math.abs(rx), 1);
+
+            //Calculations, more in depth here:
+            //https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+
+            final double frontLeftPower = (ly + lx + rx) / denominator;
+            final double backLeftPower = (ly - lx + rx) / denominator;
+            final double frontRightPower = (ly - lx - rx) / denominator;
+            final double backRightPower = (ly + lx - rx) / denominator;
+
+            final double intakePower = gamepad1.x ? 1.0d : 0.0d;
+
+            //Speed multipliers by .9, reduces speed of motor
+            //Motors get very funky when running at maximum capacity, cap their speed
+            leftFront.setPower(-frontLeftPower);
+            leftRear.setPower(-backLeftPower);
+            rightFront.setPower(frontRightPower);
+            rightRear.setPower(backRightPower);
+
+            intakeMotor.setPower(intakePower);
+            // we finished an iteration, record the time the last value was recorded for use in finding sum
+            timeStampAtLastOpModeRun = currentRunTimeStamp;
+
+            //telemetry.addData("X-value", parallelEncoder.getCurrentPosition());
+            //telemetry.addData("Y-value", perpendicularEncoder.getCurrentPosition());
+
+            telemetry.update();
+        }
+    }
+
+    public static final double DEAD_ZONE_LIMIT = 0.15d;
+
+    /**
+     * Stick return is unreliable near inside, toss signals that are less than a threshold to maintain stationary behaviour when sticks may or may not be being minimally actuated by using this method to wrap a double value.
+     * @param input
+     * @return
+     */
+    public static double respectDeadZones(double input) {
+        if (Math.abs(input) < DEAD_ZONE_LIMIT) {
+            return 0.0d;
+        } else {
+            return input;
+        }
+    }
+}
